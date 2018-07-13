85acdcf92172f2116cf8fe7a3156decd0859712e
jyung <jeremy.yung@xoom.com>
2018-07-13 16:29:02 -0700

    step 2

--- src/webparts/fileuploader/components/filetile/filetile.tsx
+++ src/webparts/fileuploader/components/filetile/filetile.tsx
@@ -28,20 +28,11 @@ export class Filetile extends React.Component<IFileTileProps> {
       FieldObjects: {},
     };
     this.handleChange = this.handleChange.bind(this);
-    this.textChange = this.textChange.bind(this);
   }
 
   public handleChange(child_data: IFieldData) {
     let fobjProperty = this.state['FieldObjects'];
     fobjProperty[child_data.FieldName] = {Id: child_data.Id, Text: child_data.Text};
-    this.setState({fobjProperty});
-  }
-
-  public textChange(event){
-    let newtext = event.target.value;
-    let fieldname = event.target.accessKey;
-    let fobjProperty = this.state['FieldObjects'];
-    fobjProperty[fieldname] = {Id: undefined, Text: newtext};
     this.setState({fobjProperty});
   }
 
@@ -64,33 +55,6 @@ export class Filetile extends React.Component<IFileTileProps> {
     });
     return infoColumns;
   }
-
-  // public makeInfoRow() {
-  //   let rowschema = this.props.fieldschema;
-  //   let infoColumns = [<td>{this.mainfile['name']}</td>];
-  //
-  //   Object.keys(rowschema).sort().forEach((key,index) =>{
-  //     let column_element = undefined;
-  //     if(Object.keys(rowschema[key]).length == 0) {
-  //       column_element = (
-  //         <td key={index.toString()}>
-  //           <input data-fieldname={key} onChange={this.textChange} accessKey={key}/>
-  //         </td>);
-  //     }
-  //     else{
-  //       column_element = (
-  //         <td key={index.toString()}>
-  //           <Miniselector
-  //             fieldname={key}
-  //             options={rowschema[key]['value']}
-  //             parentcallback = {this.handleChange}
-  //           />
-  //         </td>);
-  //     }
-  //     infoColumns.push(column_element);
-  //   });
-  //   return infoColumns;
-  // }
 
   public componentDidUpdate(){
     this.props.getFieldData({[this.mainfile['name']]: this.state['FieldObjects']});
